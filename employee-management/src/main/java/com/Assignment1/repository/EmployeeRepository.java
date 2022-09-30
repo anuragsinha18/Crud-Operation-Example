d90616f5f401560ce286925ba87f49d150f546d3
Anurag Sinha <msritanurag@gmail.com>
2022-09-30 11:18:39 +0530

    Add files via upload

---
+++ employee-management/src/main/java/com/Assignment1/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.Assignment1.repository;
+
+import com.Assignment1.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // all crud database methods
+}
